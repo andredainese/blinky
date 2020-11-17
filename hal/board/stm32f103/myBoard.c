389ae8ef14db685708a46d1e9c4a4009168186d9
Andre F. N. Dainese <andredainese@outlook.com>
2020-11-17 08:44:07 -0300

    HAL Board MKL25Z4 update: - Adding complete implementation, based on files provided by ST but adjusted for this repository. - Added logic to myBoard, it doesn't have to do anything but to compile.

---
+++ hal/board/stm32f103/myBoard.c
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2020 by Andre F. N. Dainese
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file myBoard.c
+ * @brief Header file for board specific operations, STM32F103 microcontrollers.
+ */
+
+/*******************************************************************************
+ *  INCLUDES
+ ******************************************************************************/
+#include "myBoard.h"
+
+/*******************************************************************************
+ *  PRIVATE DEFINITIONS
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  PRIVATE PROTOTYPES
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  PRIVATE VARIABLES
+ ******************************************************************************/
+
+/*******************************************************************************
+ *  PUBLIC FUNCTIONS / ROUTINES
+ ******************************************************************************/
+/**
+ * @brief Initialization routine for the board.
+ */
+void myBoard_Init(void)
+{
+  /* Nothing to do.                                                           */
+}
+
+/*******************************************************************************
+ *  PRIVATE FUNCTIONS / ROUTINES
+ ******************************************************************************/
